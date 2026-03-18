27299f806a1571fd4ec298f97a49b0b46bf4ef0f
aadityap0ap <adityabharti450ap@gmail.com>
2026-03-18 19:18:26 +0530

    s4 admin routes added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
+const {adminRouter} = require("./routes/admin")
 
 const app = express();
 
-
+app.use("/admin",adminRouter);
 app.use("/user",userRouter);
 app.use("/course",courseRouter);
 
---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const {Router} = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signin",(req,res) => {
+
+})
+
+adminRouter.post("/signup",(req,res) => {
+
+})
+
+adminRouter.post("/addCourse",(req,res) => {
+
+})
+
+adminRouter.get("/updateCourse",(req,res) => {
+
+})
+
+adminRouter.get("/course/bulk",(req,res) => {
+
+})
+
+module.exports ={
+    adminRouter : adminRouter
+}
