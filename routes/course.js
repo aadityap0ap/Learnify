8327bd0b5112d4823bae76094501982228bdcb33
aadityap0ap <adityabharti450ap@gmail.com>
2026-03-24 12:39:07 +0530

    s8 Courses EndPoint Completed

--- routes/course.js
+++ routes/course.js
@@ -1,13 +1,25 @@
 const {Router} = require("express");
+const {courseModel , purchaseModel} = require("../db");
+const { userMiddleWare } = require("../middleware/userMiddleware");
 const courseRouter = Router();
 
-courseRouter.post("/purchase",(req,res) => {
+courseRouter.post("/purchase",userMiddleWare,async(req,res) => {
+    const userId = req.userId;
+    const courseId = req.body.courseId;
 
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+    res.json({
+        message : "Course Purchased !"
+    })
 })
 
-courseRouter.get("/preview",(req,res) => {
+courseRouter.get("/preview",async(req,res) => {
+    const courses = await courseModel.find({});
     res.json({
-        message : "Course preview end point"
+        courses
     })
 })
 
